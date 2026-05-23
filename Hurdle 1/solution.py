874c6c941a9455014400b500115aa1aa5341643c
SumairIzhaan <sumairizhaan@gmail.com>
2026-05-23 11:54:14 +0530

    Update solution.py

--- Hurdle 1/solution.py
+++ Hurdle 1/solution.py
@@ -1,1 +1,19 @@
+//Defining turning right direction function
+def right():
+    turn_left()
+    turn_left()
+    turn_left()
 
+//This function will perform operation of the required task
+def perform():
+    move()
+    turn_left()
+    move()
+    right()
+    move()
+    right()
+    move()
+    turn_left()
+
+for i in range(1,7):
+    perform()
